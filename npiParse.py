413b20e1e92d6045246c20d8e6148a44d21bb78e
Stephen McCready <stephenmccready@gmail.com>
2020-09-26 01:08:44 -0400

    Update npiParse.py

--- npiParse.py
+++ npiParse.py
@@ -1,10 +1,24 @@
-# parses out the records for a single state
+# parses out the records for a single state into multiple files of 10,000 rows each
 import glob, os
 
-i = open("npidata_pfile_20050523-20200913.csv", "r")
-o = open("HI.csv", "w")
+rowcount = 0
+filecount = 0
+
+filename = "HI" + str(filecount) + ".csv"
+
+i = open("npidata_pfile_20050523-20200913.csv", "r", encoding="utf8")
+o = open(filename, "w", encoding="utf8")
 
 for row in i:
+  if rowcount > 10000:
+    o.close()
+    filecount = filecount + 1
+    filename = "HI" + str(filecount) + ".csv"
+    o = open(filename, "w", encoding="utf8")
+    rowcount = 0
   fields = row.split(",")
   if fields[23] == '"HI"' or fields[31] == '"HI"':
     o.write(row)
+    rowcount = rowcount + 1
+    
+i.close()
